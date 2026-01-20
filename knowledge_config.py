ef99d5a697416944409f7d0de713e6f3415ca8c9
Sachin Torgal <satorgal@estuate.com>
2026-01-20 14:57:50 +0530

    Code changed in accordance with gunicorn

--- knowledge_config.py
+++ knowledge_config.py
@@ -2,7 +2,7 @@ db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
 CSV_PATH = "D:\\Clari_1\\Clari_1\\Docs\\(PS)_Partner_Cases-5_Months.csv"
 WORD_PATH = "D:\\Clari_1\\Clari_1\\Docs\\Clari_solution_steps_link.docx"
-KNOWLEDGE_FOLDER = "D:\\Clari_1\\Clari_1\\mdfiles\\mdfiles_new"
+KNOWLEDGE_FOLDER = "D:\\Clari_1\\Clari_1\\mdfiles\\Data"
 
 COLLECTION_CSV = "csv_documents"
 COLLECTION_WORD = "word_documents"
